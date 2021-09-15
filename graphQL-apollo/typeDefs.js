8bd1b6d971d32b991b5fed5390c5bda5685d987b
Max Manayan <maximillian521@gmail.com>
2021-09-15 09:35:09 -0600

    basic typeDefs created

---
+++ graphQL-apollo/typeDefs.js
@@ -0,0 +1,11 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  # Types --------------------------------------------------
+
+  # Queries --------------------------------------------------
+
+  # Mutations --------------------------------------------------
+`;
+
+module.exports = { typeDefs };
