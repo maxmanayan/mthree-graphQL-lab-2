f17f171fc4966de7b30fc64e9ee3fd8e8e146fee
Max Manayan <maximillian521@gmail.com>
2021-09-15 09:44:20 -0600

    Author type created and imports fixed in server

--- graphQL-apollo/typeDefs.js
+++ graphQL-apollo/typeDefs.js
@@ -2,6 +2,11 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   # Types --------------------------------------------------
+  type Author {
+    id: ID!
+    name: String!
+    age: Int!
+  }
 
   # Queries --------------------------------------------------
 
