b951015fc9aa94e176a2627a462e157952b3e43d
Max Manayan <maximillian521@gmail.com>
2021-09-15 09:36:59 -0600

    basic resolvers setup

---
+++ graphQL-apollo/resolvers.js
@@ -0,0 +1,9 @@
+const Author = require("../models/Author");
+const Book = require("../models/Book");
+
+const resolvers = {
+  // Queries ----------------------------------
+  // Mutations ----------------------------------
+};
+
+module.exports = { resolvers };
