30a95ee05ca3fcfaa6827b86bf1019b9b89cde3d
Max Manayan <maximillian521@gmail.com>
2021-09-15 18:36:37 -0600

    homepage react hooks added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [authors, setAuthors] = useState(null);
+
+  useEffect(() => {
+    console.log("homepage");
+  }, []);
+
   return (
     <div>
       <h1>Authors</h1>
