985d84d6f3e408e8a71c98ea4163b1556b3527e9
Max Manayan <maximillian521@gmail.com>
2021-09-15 18:24:40 -0600

    Switch and Routes added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>Authors and Books</h1>
+      <Switch>
+        <Route exact path="/" />
+      </Switch>
     </div>
   );
 }
