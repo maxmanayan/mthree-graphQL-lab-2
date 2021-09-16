8099e88732767cd0f573a589a4aa403323908545
Max Manayan <maximillian521@gmail.com>
2021-09-15 18:29:23 -0600

    navigation through pages functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Authors and Books</h1>
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={Home} />
       </Switch>
     </div>
   );
